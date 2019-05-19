be18d0de04a3de7bc41699d1ca2d4fa8711afcac
Vinay <vinaykurmadas@gmail,com>
2019-05-19 11:53:35 +0530

    'makebetter2'

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ const app = express();
 // Replace the '/dist/<to_your_project_name>'
 app.use(express.static(__dirname + '/dist/library-management'));
 
-app.get('/', function(req,res) {
+app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
   res.sendFile(path.join(__dirname+ '/dist/library-management/index.html'));
 });
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
